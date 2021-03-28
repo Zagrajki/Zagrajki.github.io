4fb563e8aab987a51f729726fd8e98b0a1ebb418
Zagrajki <51937662+Zagrajki@users.noreply.github.com>
2021-03-28 19:10:51 +0200

    lab3

--- lab3js.js
+++ lab3js.js
@@ -1,11 +1,20 @@
 "use strict"
 
+    let fillData = () => {
+		let value = document.getElementById("polecenie").value;
+		if(value === ""){
+			return
+		}
+        let ele = document.getElementById('container');
+        ele.innerHTML += value;
+    }
+
 const fun1 = () => {
 	let value = document.getElementById("polecenie").value;
 	if(value === ""){
 		return
 	}
 	const newP = document.createElement("p");
-	newP,innerHTML = '${value}';
+	newP.innerHTML = '${value}';
 	$("#ToDoList").append(newP);
 }
