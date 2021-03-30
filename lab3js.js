6c0a1921650857fbdc85dadecd6ba91616c85528
Zagrajki <51937662+Zagrajki@users.noreply.github.com>
2021-03-30 21:35:36 +0200

    lab3

--- lab3js.js
+++ lab3js.js
@@ -20,3 +20,30 @@ const fun1 = () => {
 	newP.innerHTML = value;
 	$("#ToDoList").append(newP);
 }
+
+var newT = document.createElement('t');
+
+let fillData = () => {
+	let value = document.getElementById("polecenie").value;
+	if(value === ""){
+		return
+	}
+    var newP = document.createElement('p');
+	newP.innerHTML = value;
+	newP.onclick = function () {
+	if(this.style.color === 'gray'){
+		this.removeChild(newT);
+		this.style.color = 'black';
+		this.style.setProperty("text-decoration", "none");
+    } else {
+		//var newT = document.createElement('t');
+		var today = new Date();
+		newT.innerHTML = ' '+today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+		this.appendChild(newT);
+		this.style.color = 'gray';
+		this.style.setProperty("text-decoration", "line-through");
+    }
+	};
+	var table = document.getElementById('ToDoList');
+	table.appendChild(newP);
+}
