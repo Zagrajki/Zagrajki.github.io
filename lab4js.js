6e9ea081f2668b5600aced8dbe36878340163306
Zagrajki <51937662+Zagrajki@users.noreply.github.com>
2021-04-19 20:37:52 +0200

    lab4

--- lab4js.js
+++ lab4js.js
@@ -1,92 +1,143 @@
-function hashCode(str) {
-    let hash = 0;
-    if (str.length == 0) return hash;
-    for (let i = 0; i < str.length; i++) {
-        let char = str.charCodeAt(i);
-        hash = ((hash << 5) - hash) + char;
-        hash = hash & hash; // Convert to 32bit integer
-    }
-    return hash;
-}
-
 const Item = (props) => (
     <li>{props.text}</li>
 )
-
-const Box = (props) => {
-    return (
-        <input
-            type="text"
-            name="newItemValue"
-            value={props.newItemValue}
-            onChange={props.handleOnChange}
-            onKeyDown={props.handleOnKey}
-        />
-)
+var found = 0;
+var count = 0;
+const Hello = () => (<h2>Liczba znalezionych: {found}</h2>)
+const Bye = (props) => (<li>
+					<h2 style={{position: "absolute", top: "0px"}}>{props.imie}</h2>
+                    <h2 style={{position: "absolute", top: "30px"}}>{props.opis}</h2>
+                   	<h2 style={{position: "absolute", top: "90px", left: "200px"}}>{props.email}</h2>
+                    <h2 style={{position: "absolute", top: "90px"}}>{props.tagi}</h2>
+					</li>)
+const Ruler = () => {
+	for (var i = 0; i < count; i++) {
+        ObjectRow()
+    } 
 }
 
+function Glossary(props) {
+  return (
+    <dl>
+      {props.toDoList.map(item => (
+        // Without the `key`, React will fire a key warning
+        <React.Fragment key={item.id}>
+          <dt>"zycie"</dt>
+          <dd>"jest nowela"</dd>
+          <dd>{item}</dd>
+        </React.Fragment>
+      ))}
+    </dl>
+  );
+}
 
-class ToDo extends React.Component {
-
+class DlaRoot3 extends React.Component {
     state = {
-        toDoList: ["Zakupy", "Zadanie z PIWa", "Lekarz"],
-        newItemValue: "",
+        toDoList: [],
+        imie: "",
+        opis: "",
+        email: "",
+        tagi: "",
         showWarning: false
     }
     errorMessage = "Wrong entry value"
 
-    handleNewEntry = (event) => {
+    imieOnChange = (event) => {
         this.setState({
-            newItemValue: event.target.value
+            imie: event.target.value
         })
     }
-
-    handleEnter = (event) => {
-        if(event.code === "Enter"){
-            if(this.state.toDoList.includes(this.state.newItemValue)){
-                this.setState({
-                    showWarning: true
-                })
-            } else {
-                this.setState({
-                    toDoList: this.state.toDoList.concat(this.state.newItemValue),
-                    newItemValue: "",
-                    showWarning: false
-
-                })
-            }
-        } else {
-            this.setState({
-                showWarning: false
-            })
-        }
+    opisOnChange = (event) => {
+        this.setState({
+            opis: event.target.value
+        })
+    }
+    emailOnChange = (event) => {
+        this.setState({
+            email: event.target.value
+        })
+    }
+    tagiOnChange = (event) => {
+        this.setState({
+            tagi: event.target.value
+        })
+    }
+    klikniete = (event) => {
+    var a={imie:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+    	 this.setState({
+              toDoList: this.state.toDoList.concat("pp"),
+              showWarning: false
+          		});
+    ReactDOM.render(
+    <Glossary toDoList={this.state.toDoList}/>,
+    document.getElementById('root4'));
     }
     // it => ( )
     render() {
-        const myList = this.state.toDoList.map(it => (
-            <Item text={it} key={hashCode(it)} />
-        ))
 
         return (
             //React.Fragment
             <>
-                <h2>{this.props.dummyText}</h2>
-                <Box
-                    newItemValue={this.state.newItemValue}
-                    handleOnChange={this.handleNewEntry}
-                    handleOnKey={this.handleEnter}
-                />
-
-               {this.state.showWarning && <h1 style={{color: "red"}}>{this.errorMessage}</h1> }
-                <ul>
-                    {myList}
-                </ul>
+        <h2>{this.props.dummyText}</h2>
+        <p>
+        <input
+            type="text"
+            name="Imię"
+            placeholder="Imię"
+            value={this.imie}
+            onChange={this.imieOnChange}
+            onKeyDown={this.handleEnter}
+        />
+        </p>
+        <p>
+        <input
+            type="text"
+            name="Opis"
+            placeholder="Opis"
+            value={this.opis}
+            onChange={this.opisOnChange}
+            onKeyDown={this.handleEnter}
+        />
+        </p>
+        <p>
+        <input
+            type="text"
+            name="Email"
+            placeholder="Email"
+            value={this.email}
+            onChange={this.emailOnChange}
+            onKeyDown={this.handleEnter}
+        />
+        </p>
+        <p>
+        <input
+            type="text"
+            name="Tagi"
+            placeholder="Tagi"
+            value={this.tagi}
+            onChange={this.tagiOnChange}
+            onKeyDown={this.handleEnter}
+        />
+        </p>
+        <button
+        	onClick={this.klikniete}>
+        	Dodaj
+        </button>
+        {this.state.showWarning && <h1 style={{color: "red"}}>{this.errorMessage}</h1> }
             </>
         );
     }
 }
 
 ReactDOM.render(
-    <ToDo dummyText="Witaj Dniu" />,
-    document.getElementById('root')
+    <Hello />,
+    document.getElementById('root2')
+);
+ReactDOM.render(
+    <DlaRoot3 dummyText="Dodawanie" />,
+    document.getElementById('root3')
+);
+ReactDOM.render(
+    <Bye imie="a" opis="b" email="c" tagi="d"/>,
+    document.getElementById('root4')
 );
